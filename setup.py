9b079a339e2d5706341afb941186f1c73866b906
Mehul Smriti Raje <mraje@g.harvard.edu>
2019-03-25 19:43:47 -0400

    Adding cookiecutter project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data paper for EEG data collected during real-world scenario',
+    author='Mehul Smriti Raje',
+    license='MIT',
+)
